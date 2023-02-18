3b5e3a1b04765fc9a52437eaf358f4aee758da95
Yousaf Zubair <83968204+Yousafzubair991@users.noreply.github.com>
2023-02-19 04:01:05 +0500

    Add update and delete api's.

--- api/routes/student.js
+++ api/routes/student.js
@@ -3,11 +3,37 @@ const mongoose = require("mongoose");
 const router = express?.Router();
 const Student = require("../model/student");
 
+//////////////////////GET ALL STUDENTS////////////////////////
 router?.get("/", (req, res, next) => {
-  res?.status(200);
-  res.send({ message: "GET" });
+  Student?.find()
+    .then((result) => {
+      console?.log(result);
+      res?.status(200);
+      res?.json({ result: result });
+    })
+    .catch((err) => {
+      console?.log(err);
+      res?.status(500);
+      res?.json({ error: err });
+    });
+});
+//////////////////////GET STUDENT BY ID////////////////////////
+router?.get("/:studentId", (req, res, next) => {
+  const id = req?.params?.studentId;
+  Student?.findById(id)
+    .then((result) => {
+      console?.log(result);
+      res?.status(200);
+      res?.json({ result: result });
+    })
+    .catch((err) => {
+      console?.log(err);
+      res?.status(500);
+      res?.json({ error: err });
+    });
 });
 
+//////////////////////CREATE STUDENT////////////////////////
 router?.post("/", (req, res, next) => {
   console?.log("Body>>>", req?.body);
   const student = new Student({
@@ -25,6 +51,48 @@ router?.post("/", (req, res, next) => {
       console?.log(result);
       res?.status(201);
       res?.json({ message: "Student created", result: result });
+    })
+    .catch((err) => {
+      console?.log(err);
+      res?.status(500);
+      res?.json({ error: err });
+    });
+});
+
+//////////////////////DELETE STUDENT////////////////////////
+router?.delete("/:studentId", (req, res, next) => {
+  const id = req?.params?.studentId;
+  Student?.remove({ _id: id })
+
+    .then((result) => {
+      console?.log(result);
+      res?.status(200);
+      res?.json({ message: "Student deleted", result: result });
+    })
+    .catch((err) => {
+      console?.log(err);
+      res?.status(500);
+      res?.json({ error: err });
+    });
+});
+
+//////////////////////UPDATE STUDENT////////////////////////
+router?.put("/:studentId", (req, res, next) => {
+  const id = req?.params?.studentId;
+  const updateOps = {
+    name: req?.body?.name,
+    gender: req?.body?.gender,
+    class: req?.body?.class,
+    age: req?.body?.age,
+    created_at: new Date(),
+  };
+
+  Student?.update({ _id: id }, { $set: updateOps })
+
+    .then((result) => {
+      console?.log(result);
+      res?.status(200);
+      res?.json({ message: "Student updated", result: result });
     })
     .catch((err) => {
       console?.log(err);
